04a0bf52f36b3d153f78b7dfecc97b40bc1f44a8
Alexey <alexey.severyn@gmail.com>
2025-11-12 17:45:28 +0300

    Created Main class: BankCardManagementSystemApplication

---
+++ src/main/java/com/severyn/bankcard/BankCardManagementSystemApplication.java
@@ -0,0 +1,12 @@
+package com.severyn.bankcard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankCardManagementSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BankCardManagementSystemApplication.class, args);
+    }
+}
